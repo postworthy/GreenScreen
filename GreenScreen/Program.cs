bf92697053b3f8f8c314dad20c5940f3cc34d5b7
Landon Key <jolkey@JOLKEY15.southernco.com>
2016-02-05 10:07:48 -0600

    Now it is so so fast!
    Made a lot of changes to increase the speed of file processing.

--- GreenScreen/Program.cs
+++ GreenScreen/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -35,7 +36,9 @@ namespace GreenScreen
         {
             if (Directory.Exists(d))
             {
-                Directory.GetFiles(d).ToList().ForEach(f => ProcessFile(f));
+                Directory.GetFiles(d)
+                    .AsParallel()
+                    .ForAll(f => ProcessFile(f));
             }
             else
                 DisplayUsage();
@@ -56,25 +59,44 @@ namespace GreenScreen
                     Console.WriteLine("Error Processing: {0}", f);
                     return;
                 }
-                var output = new Bitmap(input.Width, input.Height);
-                var pixels =
-                    from x in Enumerable.Range(0, input.Width - 1)
-                    from y in Enumerable.Range(0, input.Height - 1)
-                    select new { x, y, v = input.GetPixel(x, y) };
-
-                foreach (var p in pixels)
+                using (Bitmap clone = new Bitmap(input.Width, input.Height, PixelFormat.Format32bppArgb))
                 {
-                    byte max = Math.Max(Math.Max(p.v.R, p.v.G), p.v.B);
-                    byte min = Math.Min(Math.Min(p.v.R, p.v.G), p.v.B);
+                    using (input)
+                    using (Graphics gr = Graphics.FromImage(clone))
+                    {
+                        gr.DrawImage(input, new Rectangle(0, 0, clone.Width, clone.Height));
+                    }
 
-                    if (p.v.G != min && (p.v.G == max || max - p.v.G < 7) && (max - min) > 20)
-                        output.SetPixel(p.x, p.y, Color.Transparent);
-                    else
-                        output.SetPixel(p.x, p.y, p.v);
+                    var data = clone.LockBits(new Rectangle(0, 0, clone.Width, clone.Height), ImageLockMode.ReadWrite, clone.PixelFormat);
+
+                    var bytes = Math.Abs(data.Stride) * clone.Height;
+                    byte[] rgba = new byte[bytes];
+                    System.Runtime.InteropServices.Marshal.Copy(data.Scan0, rgba, 0, bytes);
+
+                    var pixels = Enumerable.Range(0, rgba.Length / 4).Select(x => new {
+                        B = rgba[x * 4],
+                        G = rgba[(x * 4) + 1],
+                        R = rgba[(x * 4) + 2],
+                        A = rgba[(x * 4) + 3],
+                        MakeTransparent = new Action(() => rgba[(x * 4) + 3] = 0)
+                    });
+
+                    pixels
+                        .AsParallel()
+                        .ForAll(p =>
+                    {
+                        byte max = Math.Max(Math.Max(p.R, p.G), p.B);
+                        byte min = Math.Min(Math.Min(p.R, p.G), p.B);
+
+                        if (p.G != min && (p.G == max || max - p.G < 7) && (max - min) > 20)
+                            p.MakeTransparent();
+                    });
+
+                    System.Runtime.InteropServices.Marshal.Copy(rgba, 0, data.Scan0, bytes);
+                    clone.UnlockBits(data);
+
+                    clone.Save(o, ImageFormat.Png);
                 }
-
-                output.Save(o, System.Drawing.Imaging.ImageFormat.Png);
-
                 Console.WriteLine("Finished Processing: {0} saved as {1}", f, o);
             }
             else
